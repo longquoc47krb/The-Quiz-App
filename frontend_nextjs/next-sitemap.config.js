eaeda512f38efe369cc38bdd4181de2e3714471d
Long Nguyen Quoc <longquoc47krb@gmail.com>
2023-08-27 13:05:15 +0700

    UPDATE sitemap

--- frontend_nextjs/next-sitemap.config.js
+++ frontend_nextjs/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://example.com', // FIXME: Change to the production URL
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL, // FIXME: Change to the production URL
   generateRobotsTxt: true,
 };
